eef46c35dba0380f08c04f23b0e034bf5d218b86
James Dobson <bobberino98@gmail.com>
2019-03-24 17:44:13 -0700

    added player file

---
+++ venv/Player.py
@@ -0,0 +1,137 @@
+from pygame.sprite import Sprite
+from photo_rect import PhotoRect
+import pygame
+
+
+class Player(Sprite):
+
+    def __init__(self, screen, row, col, maze):
+        super(Player, self).__init__()
+        self.screen = screen
+        self.s_rect = screen.get_rect()
+        self.im = PhotoRect(screen, "Player_1", 18, 18)
+        self.im.rect.centerx = col*10+5
+        self.im.rect.centery = row*10+5
+        self.maze = maze
+        self.rect = self.im.rect
+        self.moving_up = False
+        self.moving_down = False
+        self.moving_right = False
+        self.moving_left = False
+        self.speed = 10
+        self.state = 1
+        self.death_state = 1
+        self.dir = ""
+        self.speed_timer = pygame.time.get_ticks()
+        self.row = row
+        self.col = col
+        self.walls = ['T', 'B', 'R', 'L', 'G', 'M', 'N', 'O', 'Y', 'Z', 'W', 'X', 'S']
+        self.dead = False
+        self.sound_timer = pygame.time.get_ticks()
+        self.finished = False
+
+    def update(self):
+        temp = ""
+        if not self.dead:
+            if self.moving_up and pygame.time.get_ticks() - self.speed_timer >= 100:
+
+                temp = self.maze.rows[self.row-1]
+                val = temp[self.col]
+                if val not in self.walls:
+                    self.im.rect.y -= self.speed
+                    self.row -= 1
+                self.im.blitme()
+                self.speed_timer = pygame.time.get_ticks()
+                self.dir = 'up'
+                temp = "Player_up_" + str(self.state)
+            elif self.moving_down and pygame.time.get_ticks() - self.speed_timer >= 100:
+
+                temp = self.maze.rows[self.row+1]
+                val = temp[self.col]
+                if val not in self.walls:
+                    self.row += 1
+                    self.im.rect.y += self.speed
+                self.im.blitme()
+                self.speed_timer = pygame.time.get_ticks()
+                self.dir = 'down'
+                temp = "Player_down_" + str(self.state)
+            elif self.moving_right and pygame.time.get_ticks() - self.speed_timer >= 100:
+
+                temp = self.maze.rows[self.row]
+                if self.col < 27:
+                    val = temp[self.col+1]
+                else:
+                    val = temp[0]
+                if val not in self.walls:
+                    self.im.rect.x += self.speed
+                    self.col += 1
+                self.im.blitme()
+                self.speed_timer = pygame.time.get_ticks()
+                self.dir = 'right'
+                temp = "Player_right_" + str(self.state)
+            elif self.moving_left and pygame.time.get_ticks() - self.speed_timer >= 100:
+
+                temp = self.maze.rows[self.row]
+                val = temp[self.col-1]
+                if val not in self.walls:
+                    self.im.rect.x -= self.speed
+                    self.col -= 1
+                self.im.blitme()
+                self.speed_timer = pygame.time.get_ticks()
+                self.dir = 'left'
+                temp = "Player_left_" + str(self.state)
+            else:
+                return temp
+            if self.rect.x < 0:
+                self.rect.centerx = 275
+                self.col = 27
+            elif self.rect.x > self.s_rect.width:
+                self.rect.centerx = 5
+                self.col = 0
+            if pygame.time.get_ticks() - self.sound_timer >= 500:
+                sound = pygame.mixer.Sound("sounds/pacman_chomp.wav")
+                sound.play()
+                self.sound_timer = pygame.time.get_ticks()
+            self.rect = self.im.rect
+            self.im = ImageRect(self.screen, temp, 18, 18)
+            self.im.rect = self.rect
+            if self.state >= 3:
+                self.state = 1
+            else:
+                self.state += 1
+        else:
+            if self.death_state == 1:
+                deathsound = pygame.mixer.Sound("sounds/pacmandies.wav")
+                deathsound.set_volume(1)
+                deathsound.play()
+            if pygame.time.get_ticks() - self.speed_timer >= 200 and self.death_state < 12:
+                temp = "Player_death_" + str(self.death_state)
+                self.death_state += 1
+                self.rect = self.im.rect
+                self.im = ImageRect(self.screen, temp, 18, 18)
+                self.im.rect = self.rect
+                self.speed_timer = pygame.time.get_ticks()
+            elif pygame.time.get_ticks() - self.speed_timer >= 200 and self.death_state >= 12:
+                self.finished = True
+
+    def create_portal(self, portal_num):
+        current_row = self.maze.rows[self.row]
+
+        if self.dir == 'right':
+            for x in range(self.col, len(current_row)):
+                if current_row[x] == 'R':
+                    current_row = current_row[:x] + str(portal_num) + current_row[x+1:]
+                    break
+        elif self.dir == 'left':
+            current_row = current_row[::-1]
+            for x in range(self.col, len(current_row)):
+                if current_row[x] == 'L':
+                    current_row = current_row[:x] + str(portal_num) + current_row[x+1:]
+                    current_row = current_row[::-1]
+                    break
+
+        self.maze.rows[self.row] = current_row
+        self.maze.update_walls()
+
+    def blitme(self):
+        self.im.blitme()
